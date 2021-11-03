d91b3f39ce9714b66b360be0b036050fffbec64d
Carlos Rangel <carlos.developer17@gmail.com>
2021-11-02 22:20:39 -0500

    Init commit

---
+++ src/components/LandingPage/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './landingPage.module.scss'
+
+const LandingPage = () => (
+  <div className={styles.container}>
+    <h1 className={styles.title}>👋 Get Started!</h1>
+    <p className={styles.subtext}>Create your components and make something cool!</p>
+  </div>
+)
+
+export default LandingPage
